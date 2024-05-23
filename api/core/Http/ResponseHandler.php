3d9534dded02cf1d13b4370450efd8805103fa3e
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-05-23 21:34:57 +0300

    Created ResponseHandler for mapping and sending responses

---
+++ api/core/Http/ResponseHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Core\Http;
+
+class ResponseHandler
+{
+    public function HandleResponse(Response $response) : void
+    {
+        http_response_code($response->statusCode);
+
+        foreach ($response->headers as $header) {
+            header($header);
+        }
+
+        if($response->data != null){
+            echo json_encode(['data' => $response->data]);
+        }
+        else if($response->error != ""){
+            echo json_encode(['error' => $response->error]);
+        }
+
+        exit();
+    }
+}
